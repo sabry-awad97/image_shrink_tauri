3da3f28146650766909c1b96de7ba3a748eed7b6
Sabry Awad <dr.sabry1997@gmail.com>
2023-08-03 09:33:13 +0300

    ✨ feat(app): Implement resizable window with menu bar
    A new `menu` module was added to the `src-tauri/src/app` directory. The `menu` module was initialized in `src-tauri/src/app/mod.rs`. Additionally, the `setup.rs` file was updated to create a resizable window with a menu bar.
    
    Changes made:
    1. A new `menu` module was added in the `src-tauri/src/app` directory.
    2. The `src-tauri/src/app/mod.rs` file was updated to include the `menu` module.
    3. The `src-tauri/src/app/setup.rs` file was updated to create a resizable window with a menu bar using the `WindowBuilder` API from the `tauri` crate.
    4. The `src-tauri/tauri.conf.json` file had some properties for a window removed, possibly for refactoring purposes.
    
    Signed-off-by: Sabry Awad <dr.sabry1997@gmail.com>

--- src-tauri/src/app/setup.rs
+++ src-tauri/src/app/setup.rs
@@ -1,7 +1,15 @@
 use std::error::Error;
-
-use tauri::App;
+use tauri::{App, WindowBuilder};
 
 pub fn init(app: &mut App) -> Result<(), Box<dyn Error>> {
+    let handle = app.handle();
+    let window_builder =
+        WindowBuilder::new(&handle, "main", tauri::WindowUrl::App("index.html".into()))
+            .title("ImageShrink")
+            .inner_size(800.0, 600.0)
+            .fullscreen(false)
+            .resizable(true);
+
+    let _window = window_builder.build()?;
     Ok(())
 }
