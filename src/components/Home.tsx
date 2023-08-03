e95a84750836b452596149980a99d9ad06a08488
Sabry Awad <dr.sabry1997@gmail.com>
2023-08-03 07:35:50 +0300

    🎉 feat(Home.tsx): Add image resizing form and UI enhancements 🔧 chore(constants): Update import in Home.tsx for appName
    Added a new feature to the Home component that allows users to resize images. The following changes were made:
    
    1. Imported the `appName` variable from the constants file to display the application name along with an image icon at the top of the page.
    
    2. Created a form to select an image for resizing. Users can now browse and select an image file.
    
    3. Added a range input for selecting the quality of the resized image. Users can adjust the quality from 0 to 100, where lower quality results in smaller file sizes.
    
    4. Enhanced the UI to display instructions for selecting the image and information about the quality range.
    
    5. Display the selected output path after resizing the image.
    
    6. Updated the import statement for `imageDirName` in Home.tsx to include the import for `appName` as well.
    
    Icons from the 'react-icons/fa' library were used to enhance the visual appeal of the application.
    
    Signed-off-by: Sabry Awad <dr.sabry1997@gmail.com>

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,6 +1,7 @@
 import { homeDir, resolve } from '@tauri-apps/api/path';
 import { useEffect, useState } from 'react';
-import { imageDirName } from '../constants';
+import { appName, imageDirName } from '../constants';
+import { FaImages } from 'react-icons/fa';
 
 const Home = () => {
   const [outputPath, setOutputPath] = useState<string>('');
@@ -17,6 +18,36 @@ const Home = () => {
 
   return (
     <div className="container center-align">
+      <h3>
+        <FaImages /> {appName}
+      </h3>
+      <p>Choose an image to resize</p>
+      <form>
+        <div className="file-field input-field">
+          <div className="btn">
+            <span>Browse</span>
+          </div>
+
+          <div className="file-path-wrapper">
+            <input
+              type="text"
+              className="file-path validate"
+              placeholder="Upload file"
+            />
+          </div>
+        </div>
+
+        <p>
+          <strong>Quality:</strong>
+          <em>The lower the quality, the smaller the file size</em>
+        </p>
+        <p className="range-field">
+          <input type="range" min="0" max="100" value="50" />
+          <span id="quality-value">50</span>
+        </p>
+
+        <input type="submit" value="Resize" className="btn black" />
+      </form>
       <div className="card output">
         <div className="card-content">
           Output Path: <span id="output-path"> {outputPath} </span>
