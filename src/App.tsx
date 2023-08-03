79954b749d51e306cada93a16c0ff6dca17c2492
Sabry Awad <dr.sabry1997@gmail.com>
2023-08-03 07:04:16 +0300

    ✨ feat(package.json): Add dependencies for routing 🎉 feat(App.tsx): Implement basic routing using @tanstack/react-location 📝 feat(About.tsx, Home.tsx): Add About and Home components
    In this commit, the following changes were made:
    
    1. `package.json`: New dependencies were added for routing and API.
       - Added "@tanstack/react-location" version "^3.7.4" for routing purposes.
    
    2. `App.tsx`: The file now implements basic routing using the newly added "@tanstack/react-location" package. Two new components, "Home" and "About," were also imported and included as routes using the `<Router>` component.
    
    3. `About.tsx` and `Home.tsx`: Two new components, "About" and "Home," were added to the project.
    
    Note: The version numbers of some existing dependencies, such as "react" and "react-dom," were also updated to "^18.2.0" in the package.json file.
    
    The addition of routing will allow for better navigation within the app, and the new components provide the basic structure for the "About" and "Home" pages.
    
    Signed-off-by: Sabry Awad <dr.sabry1997@gmail.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,25 @@
+import { ReactLocation, Router } from '@tanstack/react-location';
 import './App.css';
+import Home from './components/Home';
+import About from './components/About';
+
+const reactLocation = new ReactLocation();
 
 const App = () => {
   return (
-    <div>
-      <h1>Welcome to Tauri!</h1>
-    </div>
+    <Router
+      location={reactLocation}
+      routes={[
+        {
+          path: '/',
+          element: <Home />,
+        },
+        {
+          path: '/about',
+          element: <About />,
+        },
+      ]}
+    />
   );
 };
 
