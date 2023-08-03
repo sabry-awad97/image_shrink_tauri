4acdfbf2b9eb6c6eb38f588b8beddfb4f1f9fcbb
Sabry Awad <dr.sabry1997@gmail.com>
2023-08-03 12:58:50 +0300

    🔧 refactor(setup.rs): Enhance menu handling and code structure
    Restructured the code within `src-tauri/src/app/setup.rs` for better menu handling and readability. The following changes were made:
    
    - Created a `file_submenu` to group file-related menu items under the "File" menu.
    - Added a "Reload" menu item with accelerator key "CmdOrCtrl+R" to the "File" menu.
    - Implemented menu event handling using the `on_menu_event` method, allowing for custom behavior when menu items are clicked.
    - Refactored the window builder to include the new menu and improved the code formatting.
    
    These changes enhance the organization of menu items and provide a better-defined structure for handling menu events.
    
    Signed-off-by: Sabry Awad <dr.sabry1997@gmail.com>

--- src-tauri/src/app/setup.rs
+++ src-tauri/src/app/setup.rs
@@ -1,15 +1,40 @@
 use std::error::Error;
-use tauri::{App, WindowBuilder};
+use tauri::{App, CustomMenuItem, Menu, MenuItem, Submenu, WindowBuilder};
 
 pub fn init(app: &mut App) -> Result<(), Box<dyn Error>> {
+    let file_submenu = Submenu::new(
+        "File",
+        Menu::with_items([
+            CustomMenuItem::new("reload", "Reload")
+                .accelerator("CmdOrCtrl+R")
+                .into(),
+            MenuItem::Separator.into(),
+            MenuItem::Quit.into(),
+        ]),
+    );
+    let menu = Menu::new().add_submenu(file_submenu);
+
     let handle = app.handle();
-    let window_builder =
+    let mut window_builder =
         WindowBuilder::new(&handle, "main", tauri::WindowUrl::App("index.html".into()))
             .title("ImageShrink")
             .inner_size(800.0, 600.0)
             .fullscreen(false)
             .resizable(true);
 
-    let _window = window_builder.build()?;
+    window_builder = window_builder.menu(menu);
+
+    let win = window_builder.build()?;
+    let win_clone = win.clone();
+    win.on_menu_event(move |event| {
+        let id = event.menu_item_id();
+        match id {
+            "reload" => {
+                win_clone.eval("window.location.reload()").unwrap();
+            }
+            "about" => {}
+            _ => {}
+        }
+    });
     Ok(())
 }
